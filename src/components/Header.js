8a5d7a44b028a4a59b8b99ba35165a137ae25998
yan zhong <jackyan63@gmail.com>
2022-01-10 22:56:10 -0500

    more header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,12 +6,14 @@ import Image from 'next/image'
 function Header() {
     return (
         <header>
-            <div>
+            <div className="flex items-center bg-amazon_blue">
                 <div>
                     <Image 
                         src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
                         width={150}
                         height={40}
+                        objectFit="contain"
+                        className="cursor-pointer"
                     />
                 </div>
             </div>
