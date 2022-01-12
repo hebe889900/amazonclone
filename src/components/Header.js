c5336eb9f9441ff2c4d19f00ce6210b407deb788
yan zhong <jackyan63@gmail.com>
2022-01-11 23:01:52 -0500

    so far so good

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ import {
 function Header() {
     return (
         <header>
-            <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+            <div className="sm:flex-wrap md:flex items-center bg-amazon_blue p-1 flex-grow py-2">
                 <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
                     <Image 
                         src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
@@ -21,9 +21,28 @@ function Header() {
                         className="cursor-pointer"
                     />
                 </div>
-                <div className="hidden sm:flex items-center rounded-md h-10 flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
+                <div className="flex items-center rounded-md h-10 flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
                     <input className='p-2 h-full w-6 flex-grow flex-shrink rounted-l-md focus:outline-none px-4' type="text"/>
                     <SearchIcon className='h-12 p-4' />
+                </div>
+                <div  className="text-white flex items-center text-xs space-x-6 mx-6">
+                    <div className="link">
+                        <p>Hello Jack</p>
+                        <p className='font-extrabold md:text-sm'>Account & Lists</p>
+                    </div>
+
+                    <div className="link">
+                        <p>Return</p>
+                        <p className='font-extrabold md:text-sm'>& Orders</p>
+                    </div>
+
+                    <div className="relative link flex item-center">
+                        <span className='absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold'>
+                        4
+                        </span>
+                        <ShoppingCartIcon className="h-10" />
+                        <p className='hidden md:inline font-extrabold md:text-sm mt-2'>Basket</p>
+                    </div>
                 </div>
             </div>
             <div></div>
